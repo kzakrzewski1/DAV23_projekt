fe4764cbd41a0bb4da0a1b3aeee19b5dce3cb4af
kzakrzewski1 <117981650+kzakrzewski1@users.noreply.github.com>
2023-05-28 17:23:55 +0200

    simple plots (updated)

--- cases_deaths.py
+++ cases_deaths.py
@@ -11,28 +11,37 @@ ax2 = ax1.twinx()
 
 ax1.plot(
     can_df["date"],
-    can_df["new_cases_smoothed"],
-    c="blue",
-    label="New cases",
-    linewidth=2,
-)
-ax2.plot(
-    can_df["date"],
     can_df["new_deaths_smoothed"],
     c="red",
     label="Deaths",
     linewidth=2,
 )
+ax2.plot(
+    can_df["date"],
+    can_df["new_cases_smoothed"],
+    c="blue",
+    label="New Cases",
+    linewidth=2,
+)
 
-plt.grid(True)
+ax1.set_ylim(-10, 190)
+ax2.set_ylim(-2432, 45600)
+
+ax2.set_yticks(range(0,45600,6000))
+ax2.set_yticklabels(["0", "6K", "12K", "18K", "24K", "30K", "36K", "42K"])
+
+ax1.set_axisbelow(True)
+ax1.set_facecolor("#F0F0F0")
+ax1.grid(color = 'white', linewidth = 1.6)
+ax1.grid(which = "minor", color = 'white', linewidth = 0.5)
 
 h1, l1 = ax1.get_legend_handles_labels()
 h2, l2 = ax2.get_legend_handles_labels()
 
 ax2.legend(h1 + h2, l1 + l2, fontsize=20)
 
-ax1.set_title("Daily New Cases per 10000 vs Daily Deaths per 1000.000, Canada", size=25)
-ax1.set_xlabel("Date", size=18)
-ax1.set_ylabel("Number of New Cases\n", size=18)
-ax2.set_ylabel("Number of Deaths\n", size=18)
+ax1.set_title("Daily New Cases vs Daily Deaths", size=25, pad = 25)
+ax1.set_xlabel("Date", size=18, labelpad = 15)
+ax1.set_ylabel("Number of Deaths\n", size=18)
+ax2.set_ylabel("Number of New Cases\n", size=18, labelpad = 25)
 plt.show()
